88a2b16425e99a3da8a05461ef6da403bbbf7be3
Geir Gåsodden <geir.gasodden@gmail.com>
2023-11-01 12:03:37 +0100

    b fikser assets prefix så det funker lokalt

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined
 }
 
 module.exports = nextConfig
