531c3758dc3be27b75faaa3517a5ae8b82addbe1
Geir Gåsodden <geir.gasodden@gmail.com>
2023-11-03 12:57:32 +0100

    r fikser mock for featuretoggles i lokal dev

--- src/pages/api/features.ts
+++ src/pages/api/features.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { logger } from '@navikt/next-logger';
 import { getDefinitions } from '@unleash/nextjs';
+import { featureMocks } from './mocks/features';
 
 async function features(req: NextApiRequest, res: NextApiResponse) {
+    const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
     try {
-        const definitions = await getDefinitions();
+        const definitions = brukerMock ? featureMocks : await getDefinitions();
         return res.status(200).json(definitions.features || []);
     } catch (error) {
         logger.error(error);
--- src/pages/api/mocks/features.ts
+++ src/pages/api/mocks/features.ts
@@ -1,117 +1,119 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+export const featureMocks = {
+    version: 1,
+    features: [
+        {
+            name: 'arbeidssokerregistrering.fss.ny-ingress',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: [],
+        },
+        {
+            name: 'arbeidssokerregistrering.eksperimenter.videresend-til-aia',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: [],
+        },
+        {
+            name: 'arbeidssokerregistrering.ingen_kvittering',
+            type: 'release',
+            enabled: true,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: [],
+        },
+        {
+            name: 'arbeidssokerregistrering.kontaktopplysninger',
+            type: 'release',
+            enabled: true,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                },
+            ],
+            variants: null,
+        },
+        {
+            name: 'arbeidssokerregistrering.kunngjoring',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: null,
+        },
+        {
+            name: 'arbeidssokerregistrering.nedetid',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: null,
+        },
+        {
+            name: 'arbeidssokerregistrering.mer-oppfolging',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'default',
+                    parameters: {},
+                },
+            ],
+            variants: null,
+        },
+        {
+            name: 'arbeidssokerregistrering.ny-ingress',
+            type: 'release',
+            enabled: false,
+            stale: false,
+            strategies: [
+                {
+                    name: 'byCluster',
+                    parameters: {
+                        cluster: 'dev-gcp,dev-sbs,prod-gcp,prod-sbs',
+                    },
+                    constraints: [],
+                },
+            ],
+            variants: [],
+        },
+    ],
+}
+
 const featureToggles = (req: NextApiRequest, res: NextApiResponse): void => {
-    res.status(200).json({
-        version: 1,
-        features: [
-            {
-                name: 'arbeidssokerregistrering.fss.ny-ingress',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: [],
-            },
-            {
-                name: 'arbeidssokerregistrering.eksperimenter.videresend-til-aia',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: [],
-            },
-            {
-                name: 'arbeidssokerregistrering.ingen_kvittering',
-                type: 'release',
-                enabled: true,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: [],
-            },
-            {
-                name: 'arbeidssokerregistrering.kontaktopplysninger',
-                type: 'release',
-                enabled: true,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                    },
-                ],
-                variants: null,
-            },
-            {
-                name: 'arbeidssokerregistrering.kunngjoring',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: null,
-            },
-            {
-                name: 'arbeidssokerregistrering.nedetid',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: null,
-            },
-            {
-                name: 'arbeidssokerregistrering.mer-oppfolging',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'default',
-                        parameters: {},
-                    },
-                ],
-                variants: null,
-            },
-            {
-                name: 'arbeidssokerregistrering.ny-ingress',
-                type: 'release',
-                enabled: false,
-                stale: false,
-                strategies: [
-                    {
-                        name: 'byCluster',
-                        parameters: {
-                            cluster: 'dev-gcp,dev-sbs,prod-gcp,prod-sbs',
-                        },
-                        constraints: [],
-                    },
-                ],
-                variants: [],
-            },
-        ],
-    });
+    res.status(200).json(featureMocks);
 };
 
 export default featureToggles;
