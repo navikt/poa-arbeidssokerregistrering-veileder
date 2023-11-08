68f0e1638fba40fe530a35ecd40e9e182cfafc30
Jostein Holje <jostein.holje@nav.no>
2023-11-08 13:26:47 +0100

    Lag obo token for veilarbregistrering

---
+++ src/auth/oboTokenDings.ts
@@ -0,0 +1,66 @@
+import { Issuer, TokenSet } from 'openid-client';
+import { JWK } from 'node-jose';
+import { logger } from '@navikt/next-logger';
+
+export interface ExchangeToken {
+    (token: string, targetApp: string): Promise<TokenSet>;
+}
+
+const assert = <T extends any>(value: T | undefined | null, msg?: string): T => {
+    if (!value) {
+        throw new Error(msg || 'Value is missing');
+    }
+
+    return value;
+};
+
+const getAzureAdOptions = () => {
+    const clientId = assert(process.env.AZURE_APP_CLIENT_ID, 'AZURE_APP_CLIENT_ID is missing');
+    const discoveryUrl = assert(process.env.AZURE_APP_WELL_KNOWN_URL, 'AZURE_APP_WELL_KNOWN_URL is missing');
+    const privateJwk = assert(process.env.AZURE_APP_JWK, 'AZURE_APP_JWK is missing');
+
+    return {
+        clientId,
+        discoveryUrl,
+        privateJwk,
+    };
+};
+export interface OboAuth {
+    getOboToken: ExchangeToken;
+}
+
+const createOboTokenDings = async (): Promise<OboAuth> => {
+    const { clientId, discoveryUrl, privateJwk } = getAzureAdOptions();
+    const issuer = await Issuer.discover(discoveryUrl);
+    const client = new issuer.Client(
+        {
+            client_id: clientId,
+            token_endpoint_auth_method: 'private_key_jwt',
+            token_endpoint_auth_signing_alg: 'RS256',
+            response_types: ['code'],
+        },
+        JWK.asKey(privateJwk),
+    );
+
+    return {
+        async getOboToken(accessToken, scope) {
+            try {
+                return client.grant({
+                    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                    client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+                    requested_token_use: 'on_behalf_of',
+                    scope,
+                    assertion: accessToken,
+                    subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+                    subject_token: accessToken,
+                    audience: scope,
+                });
+            } catch (err: unknown) {
+                logger.error(err, `Feil ved generering av OBO-token: ${err}`);
+                return Promise.reject(err);
+            }
+        },
+    };
+};
+
+export default createOboTokenDings;
