d30d313af19d20cd0afadc086a5ee0a6a20cc1f8
Geir Gåsodden <geir.gasodden@gmail.com>
2023-11-09 06:56:01 +0100

    r sender med fnr til fullfør registrering

--- src/components/skjema/registrer-arbeidssoker-knapp.tsx
+++ src/components/skjema/registrer-arbeidssoker-knapp.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 
 import useSprak from '../../hooks/useSprak';
 import { useRegistrering } from '../../contexts/registrering-context';
+import { useParamsFromContext } from '../../contexts/params-from-context';
 
 import { fetcher as api } from '../../lib/api-utils';
 import { loggFlyt } from '../../lib/amplitude';
@@ -11,7 +12,7 @@ import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-spra
 import { hentRegistreringFeiletUrl } from '../../lib/hent-registrering-feilet-url';
 import { FullforRegistreringResponse } from '../../model/registrering';
 import { OppgaveRegistreringstype } from '../../model/feilsituasjonTyper';
-import byggRegistrerArbeidssokerPayload from '../../lib/bygg-registrer-arbeidssoker-payload'
+import byggRegistrerArbeidssokerPayload from '../../lib/bygg-registrer-arbeidssoker-payload';
 
 const TEKSTER: Tekster<string> = {
     nb: {
@@ -25,47 +26,42 @@ const TEKSTER: Tekster<string> = {
 
 export const RegistrerArbeidssokerKnapp = () => {
     const router = useRouter();
-    const [isDisabled, setIsDisabled] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false);
     const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
-    const { setDoValidate, doValidate, isValid, registrering } = useRegistrering()
+    const { setDoValidate, doValidate, isValid, registrering } = useRegistrering();
+    const { fnr } = useParamsFromContext();
 
-    async function registrerArbeidssoker () {
-      setIsDisabled(true)
-      setDoValidate(true)
-      if (isValid) {
-        const body = byggRegistrerArbeidssokerPayload(registrering)
-        const response: FullforRegistreringResponse = await api(
-          '/api/fullforregistrering/',
-          {
-              method: 'post',
-              body: JSON.stringify(body),
-          },
-        );
-        const feiltype = response.type;
+    async function registrerArbeidssoker() {
+        setIsDisabled(true);
+        setDoValidate(true);
+        if (isValid) {
+            const body = byggRegistrerArbeidssokerPayload(registrering);
+            const registreringUrl = `/api/fullforregistrering?fnr=${fnr}`;
+            const response: FullforRegistreringResponse = await api(registreringUrl, {
+                method: 'post',
+                body: JSON.stringify(body),
+            });
+            const feiltype = response.type;
 
-        if (feiltype) {
-          loggFlyt({ hendelse: 'Får ikke fullført registreringen', aarsak: feiltype });
-          return router.push(hentRegistreringFeiletUrl(feiltype, OppgaveRegistreringstype.REGISTRERING));
+            if (feiltype) {
+                loggFlyt({ hendelse: 'Får ikke fullført registreringen', aarsak: feiltype });
+                return router.push(hentRegistreringFeiletUrl(feiltype, OppgaveRegistreringstype.REGISTRERING));
+            }
+
+            return router.push('/kvittering');
         }
-
-        return router.push('/kvittering')
-      }
     }
 
     useEffect(() => {
-      if (doValidate && isValid) {
-        setIsDisabled(false)
-      }
-    }, [doValidate, isValid])
+        if (doValidate && isValid) {
+            setIsDisabled(false);
+        }
+    }, [doValidate, isValid]);
 
     return (
-        <div className='flex justify-end'>
-            <Button
-              variant="primary"
-              onClick={() => registrerArbeidssoker()}
-              disabled={isDisabled}
-            >
-              {tekst('registrer')}
+        <div className="flex justify-end">
+            <Button variant="primary" onClick={() => registrerArbeidssoker()} disabled={isDisabled}>
+                {tekst('registrer')}
             </Button>
         </div>
     );
