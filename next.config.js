5704aaabc4dea8c267bf5ce95d72be2119a247b3
Jostein Holje <jostein.holje@nav.no>
2023-11-10 15:11:02 +0100

    Prøv redirects via nais

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 const decoraturUrl = process.env.DECORATOR_URL;
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/internarbeidsflatedecorator/:path*',
-                destination: `${decoraturUrl}/internarbeidsflatedecorator/:path*`,
-            },
-        ];
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/internarbeidsflatedecorator/:path*',
+    //             destination: `${decoraturUrl}/internarbeidsflatedecorator/:path*`,
+    //         },
+    //     ];
+    // },
     reactStrictMode: true,
     output: 'standalone',
     assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined,
