b5f8cc330ad4962fad7720f9dba5f409ca44e947
Jostein Holje <jostein.holje@nav.no>
2023-11-14 12:36:30 +0100

    Riktige context ruter

--- src/pages/api/modiacontextholder/context/aktivbruker.ts
+++ src/pages/api/modiacontextholder/context/aktivbruker.ts
@@ -1,5 +1,5 @@
 import { withAuthenticatedApi } from '../../../../auth/withAuthentication';
 import { lagModiaContextKall } from '../../hent-modia-context';
 
-const url = `${process.env.MODIACONTEXTHOLDER_URL}/modiacontextholder/api/aktivbruker`;
+const url = `${process.env.MODIACONTEXTHOLDER_URL}/modiacontextholder/api/context/aktivbruker`;
 export default withAuthenticatedApi(lagModiaContextKall(url));
--- src/pages/api/modiacontextholder/context/aktivenhet.ts
+++ src/pages/api/modiacontextholder/context/aktivenhet.ts
@@ -1,5 +1,5 @@
 import { withAuthenticatedApi } from '../../../../auth/withAuthentication';
 import { lagModiaContextKall } from '../../hent-modia-context';
 
-const url = `${process.env.MODIACONTEXTHOLDER_URL}/modiacontextholder/api/aktivenhet`;
+const url = `${process.env.MODIACONTEXTHOLDER_URL}/modiacontextholder/api/context/aktivenhet`;
 export default withAuthenticatedApi(lagModiaContextKall(url));
