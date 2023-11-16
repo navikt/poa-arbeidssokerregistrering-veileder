243b75aa88b53f3b3cf8d54bda080b5cbf72b8dc
Jostein Holje <jostein.holje@nav.no>
2023-11-16 14:39:52 +0100

    veilarbperson og veilarbveileder via paw-proxy

--- src/pages/api/veilarbperson/[...slug].ts
+++ src/pages/api/veilarbperson/[...slug].ts
@@ -1,11 +1,13 @@
 import { withAuthenticatedApi } from '../../../auth/withAuthentication';
 import { createProxyCall } from '../../../lib/proxy-api-kall';
-import { getHeaders, getVeilarboppfolgingToken, getVeilarbpersonToken } from '../../../lib/next-api-handler';
+import { getHeaders, getPawProxyToken } from '../../../lib/next-api-handler';
 
-const url = `${process.env.VEILARBPERSON_URL}/api/[slug]`;
+// const url = `${process.env.VEILARBPERSON_URL}/api/[slug]`;
+const url = `${process.env.PAW_PROXY_URL}/veilarbperson/api/[slug]`;
 const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
 export default withAuthenticatedApi(
     createProxyCall(async (req, callId) => {
-        return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbpersonToken(req), callId);
+        // return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbpersonToken(req), callId);
+        return brukerMock ? getHeaders('token', callId) : getHeaders(await getPawProxyToken(req), callId);
     }, url),
 );
--- src/pages/api/veilarbveileder/[...slug].ts
+++ src/pages/api/veilarbveileder/[...slug].ts
@@ -1,16 +1,13 @@
 import { withAuthenticatedApi } from '../../../auth/withAuthentication';
 import { createProxyCall } from '../../../lib/proxy-api-kall';
-import {
-    getHeaders,
-    getVeilarboppfolgingToken,
-    getVeilarbpersonToken,
-    getVeilarbveilederToken,
-} from '../../../lib/next-api-handler';
+import { getHeaders, getPawProxyToken } from '../../../lib/next-api-handler';
 
-const url = `${process.env.VEILARBVEILEDER_URL}/api/[slug]`;
+// const url = `${process.env.VEILARBVEILEDER_URL}/api/[slug]`;
+const url = `${process.env.PAW_PROXY_URL}/veilarbveileder/api/[slug]`;
 const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
 export default withAuthenticatedApi(
     createProxyCall(async (req, callId) => {
-        return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbveilederToken(req), callId);
+        // return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbveilederToken(req), callId);
+        return brukerMock ? getHeaders('token', callId) : getHeaders(await getPawProxyToken(req), callId);
     }, url),
 );
