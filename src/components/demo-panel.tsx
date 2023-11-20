a8c907a2e9937377261057b5925ebe596647bd77
Geir Gåsodden <geir.gasodden@gmail.com>
2023-11-20 14:03:36 +0100

    r rydder i demovalg

--- src/components/demo-panel.tsx
+++ src/components/demo-panel.tsx
@@ -1,6 +1,6 @@
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
-import { Button, Heading, Panel } from '@navikt/ds-react';
+import { Button, Heading, Box } from '@navikt/ds-react';
 
 interface Props {
     brukerMock?: boolean;
@@ -9,121 +9,103 @@ interface Props {
 function DemoPanel({ brukerMock }: Props) {
     const router = useRouter();
     if (!brukerMock) return null;
-    const { visGammelDineOpplysninger } = router.query;
 
     return (
-        <Panel className="text-center mt-8">
-            <Heading level="2" size="xlarge" className={'mbm'}>
+        <Box className="mt-8 flex flex-col">
+            <Heading level="1" size="medium" className={'mbm'}>
                 Demovalg
             </Heading>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Panel className={'mxxs'} style={{ backgroundColor: 'var(--a-orange-50)' }}>
-                    <div className="text-center">
-                        <Heading level="3" size="large">
-                            Velg side
-                        </Heading>
-                        <div className="text-center">
-                            <p>
-                                <NextLink href={`/registrering-arbeidssoker`} passHref locale={false}>
-                                    <Button>Arbeidssøker registrering</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/kvittering-arbeidssoker/" passHref locale={false}>
-                                    <Button variant="secondary">Kvittering registrert arbeidssøker</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href={`/registrering-mer-sykmeldtoppfolging`} passHref locale={false}>
-                                    <Button variant="secondary">Mer sykmeldt oppfølging</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/kvittering-mer-sykmeldtoppfolging/" passHref locale={false}>
-                                    <Button variant="secondary">
-                                        Kvittering registrert for mer sykmeldtoppfølging
-                                    </Button>
-                                </NextLink>
-                            </p>
-                            <Heading level="4" size="medium">
-                                Stoppsituasjoner
-                            </Heading>
-                            <p>
-                                <NextLink
-                                    href="/veiledning/registrering/mangler-arbeidstillatelse/"
-                                    passHref
-                                    locale={false}
-                                >
-                                    <Button variant="secondary">Mangler arbeidstillatelse</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/veiledning/registrering/utvandret/" passHref locale={false}>
-                                    <Button variant="secondary">Utvandret</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/veiledning/allerede-registrert/" passHref locale={false}>
-                                    <Button variant="secondary">Allerede registrert</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/veiledning/sperret/" passHref locale={false}>
-                                    <Button variant="secondary">Sperret</Button>
-                                </NextLink>
-                            </p>
-                            <Heading level="4" size="medium">
-                                Feilmeldinger
-                            </Heading>
-                            <p>
-                                <NextLink href="/vedlikehold/" passHref locale={false}>
-                                    <Button variant="secondary">Vedlikehold</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/feil/" passHref locale={false}>
-                                    <Button variant="secondary">Feil</Button>
-                                </NextLink>
-                            </p>
-                            <Heading level="4" size="medium">
-                                Reaktivering
-                            </Heading>
-                            <p>
-                                <NextLink href="/registrering-reaktivering" passHref locale={false}>
-                                    <Button variant="secondary">Reaktivering</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/veiledning/reaktivering/utvandret/" passHref locale={false}>
-                                    <Button variant="secondary">Utvandret Reaktivering</Button>
-                                </NextLink>
-                            </p>
-                            <p>
-                                <NextLink href="/kvittering-reaktivering/" passHref locale={false}>
-                                    <Button variant="secondary">Kvittering Reaktivering</Button>
-                                </NextLink>
-                            </p>
-                        </div>
-                    </div>
-                </Panel>
-                <Panel className={'mxxs'} style={{ backgroundColor: 'var(--a-orange-50)' }}>
-                    <Heading level="3" size="large">
-                        Velg tekstversjon
-                    </Heading>
+            <Box className={'bg-orange-300'}>
+                <Heading level="1" size="small">
+                    Velg side
+                </Heading>
+                <Box className="flex justify-between">
+                    <NextLink href={`/registrering-arbeidssoker`} passHref locale={false}>
+                        <Button>Arbeidssøker registrering</Button>
+                    </NextLink>
+                    <NextLink href="/kvittering-arbeidssoker/" passHref locale={false}>
+                        <Button variant="secondary">Kvittering registrert arbeidssøker</Button>
+                    </NextLink>
+
                     <p>
-                        <NextLink
-                            href={`/${visGammelDineOpplysninger ? '' : '?visGammelDineOpplysninger=true'}`}
-                            passHref
-                            locale={false}
-                        >
-                            <Button variant="secondary">
-                                Vis {visGammelDineOpplysninger ? 'ny' : 'gammel'} introtekst
-                            </Button>
+                        <NextLink href={`/registrering-mer-sykmeldtoppfolging`} passHref locale={false}>
+                            <Button variant="secondary">Mer sykmeldt oppfølging</Button>
                         </NextLink>
                     </p>
-                </Panel>
-            </div>
-        </Panel>
+                    <p>
+                        <NextLink href="/kvittering-mer-sykmeldtoppfolging/" passHref locale={false}>
+                            <Button variant="secondary">Kvittering registrert for mer sykmeldtoppfølging</Button>
+                        </NextLink>
+                    </p>
+                </Box>
+                <Box>
+                    <Heading level="2" size="small">
+                        Stoppsituasjoner
+                    </Heading>
+                    <Box className="flex justify-between">
+                        <p>
+                            <NextLink
+                                href="/veiledning/registrering/mangler-arbeidstillatelse/"
+                                passHref
+                                locale={false}
+                            >
+                                <Button variant="secondary">Mangler arbeidstillatelse</Button>
+                            </NextLink>
+                        </p>
+                        <p>
+                            <NextLink href="/veiledning/registrering/utvandret/" passHref locale={false}>
+                                <Button variant="secondary">Utvandret</Button>
+                            </NextLink>
+                        </p>
+                        <p>
+                            <NextLink href="/veiledning/allerede-registrert/" passHref locale={false}>
+                                <Button variant="secondary">Allerede registrert</Button>
+                            </NextLink>
+                        </p>
+                        <p>
+                            <NextLink href="/veiledning/sperret/" passHref locale={false}>
+                                <Button variant="secondary">Sperret</Button>
+                            </NextLink>
+                        </p>
+                    </Box>
+                </Box>
+                <Box>
+                    <Heading level="4" size="medium">
+                        Feilmeldinger
+                    </Heading>
+                    <p>
+                        <NextLink href="/vedlikehold/" passHref locale={false}>
+                            <Button variant="secondary">Vedlikehold</Button>
+                        </NextLink>
+                    </p>
+                    <p>
+                        <NextLink href="/feil/" passHref locale={false}>
+                            <Button variant="secondary">Feil</Button>
+                        </NextLink>
+                    </p>
+                </Box>
+                <Box>
+                    <Heading level="4" size="medium">
+                        Reaktivering
+                    </Heading>
+                    <p>
+                        <NextLink href="/registrering-reaktivering" passHref locale={false}>
+                            <Button variant="secondary">Reaktivering</Button>
+                        </NextLink>
+                    </p>
+                    <p>
+                        <NextLink href="/veiledning/reaktivering/utvandret/" passHref locale={false}>
+                            <Button variant="secondary">Utvandret Reaktivering</Button>
+                        </NextLink>
+                    </p>
+                    <p>
+                        <NextLink href="/kvittering-reaktivering/" passHref locale={false}>
+                            <Button variant="secondary">Kvittering Reaktivering</Button>
+                        </NextLink>
+                    </p>
+                </Box>
+            </Box>
+        </Box>
     );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const brukerMock = process.env.NEXT_PUBLIC_ENABLE_MOCK === 'enabled';
+
 export default function Document() {
     return (
         <Html lang="no">
