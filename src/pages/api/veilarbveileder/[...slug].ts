76cdd31a31cbe134d16d2a4f716cee1405c1b3aa
Jostein Holje <jostein.holje@nav.no>
2023-11-21 10:06:54 +0100

    Ikke bruk paw-proxy

--- src/pages/api/veilarbveileder/[...slug].ts
+++ src/pages/api/veilarbveileder/[...slug].ts
@@ -1,13 +1,11 @@
 import { withAuthenticatedApi } from '../../../auth/withAuthentication';
 import { createProxyCall } from '../../../lib/proxy-api-kall';
-import { getHeaders, getPawProxyToken } from '../../../lib/next-api-handler';
+import { getHeaders, getVeilarbveilederToken } from '../../../lib/next-api-handler';
 
-// const url = `${process.env.VEILARBVEILEDER_URL}/api/[slug]`;
-const url = `${process.env.PAW_PROXY_URL}/veilarbveileder/api/[slug]`;
+const url = `${process.env.VEILARBVEILEDER_URL}/api/[slug]`;
 const brukerMock = process.env.ENABLE_MOCK === 'enabled';
 export default withAuthenticatedApi(
     createProxyCall(async (req, callId) => {
-        // return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbveilederToken(req), callId);
-        return brukerMock ? getHeaders('token', callId) : getHeaders(await getPawProxyToken(req), callId);
+        return brukerMock ? getHeaders('token', callId) : getHeaders(await getVeilarbveilederToken(req), callId);
     }, url),
 );
