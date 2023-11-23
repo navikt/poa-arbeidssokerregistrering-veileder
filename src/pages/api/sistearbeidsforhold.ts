db1af054856da1f3574cd63c7440e2b208af0fb4
Jostein Holje <jostein.holje@nav.no>
2023-11-23 10:14:05 +0100

    Litt bedre feilhåndtering i /api/sistearbeidsforhold

--- src/pages/api/sistearbeidsforhold.ts
+++ src/pages/api/sistearbeidsforhold.ts
@@ -19,15 +19,32 @@ const sisteArbeidsforhold = async (req: NextApiRequest, res: NextApiResponse<any
             ? getHeaders('token', callId)
             : getHeaders(await getVeilarbregistreringToken(req), callId);
         logger.info(`Starter kall callId: ${callId} mot ${url}`);
-        const { styrk } = await fetch(`${url}?fnr=${fnr}`, {
+
+        const { styrk, status } = await fetch(`${url}?fnr=${fnr}`, {
             headers,
-        }).then((res) => res.json());
+        }).then((res) => {
+            if (!res.ok) {
+                logger.error(`Feil med callId: ${callId}, respons: ${res.status} ${res.statusText}`);
+                return { styrk: undefined, status: res.status };
+            }
+            return res.json();
+        });
+
+        if (!styrk) {
+            return res.status(status).end();
+        }
+
         logger.info(`Kall med callId: ${callId} mot ${url} ferdig`);
         const pamJanzzUrl = `${process.env.PAM_JANZZ_URL}/kryssklassifiserMedKonsept?kodeForOversetting=${styrk}`;
         logger.info(`Starter kall callId: ${callId} mot ${pamJanzzUrl}`);
         const { konseptMedStyrk08List } = await fetch(pamJanzzUrl, {
             headers,
-        }).then((res) => res.json());
+        }).then((res) => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
+            return res.json();
+        });
         logger.info(`Kall med callId: ${callId} mot ${pamJanzzUrl} ferdig`);
         res.json(konseptMedStyrk08List[0]);
     } catch (e) {
