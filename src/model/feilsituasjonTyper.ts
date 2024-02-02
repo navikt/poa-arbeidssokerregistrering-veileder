aba674fece63d710bd8025bf1a844e9d799b60aa
Geir Gåsodden <geir.gasodden@gmail.com>
2024-02-02 06:44:55 +0100

    c legger BRUKER_KAN_IKKE_REAKTIVERES til Feiltype enum

--- src/model/feilsituasjonTyper.ts
+++ src/model/feilsituasjonTyper.ts
@@ -1,6 +1,7 @@
 export enum Feiltype {
     UTVANDRET = 'utvandret',
     MANGLER_ARBEIDSTILLATELSE = 'mangler-arbeidstillatelse',
+    BRUKER_KAN_IKKE_REAKTIVERES = 'kan-ikke-reaktiveres',
 }
 
 export enum OppgaveRegistreringstype {
