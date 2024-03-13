2fa213ec0bcef0164ac55d90870ae747f76e3d54
Geir Gåsodden <geir.gasodden@gmail.com>
2024-03-13 10:56:52 +0100

    r oppdaterer valgene på forsiden

--- src/components/velg-registreringstype.tsx
+++ src/components/velg-registreringstype.tsx
@@ -1,18 +1,40 @@
-import { Heading, LinkPanel } from '@navikt/ds-react';
+import { Heading, List, Button } from '@navikt/ds-react';
+import { useRouter } from 'next/router';
 
 function VelgRegistreringstype() {
+    const router = useRouter();
     return (
         <>
-            <Heading level="1" size="large" className="mb-8">
-                Velg registreringstype
+            <Heading level="1" size="large" className="mb-8 text-center">
+                Velg hvordan personen skal registreres
             </Heading>
             <div className="flex flex-row justify-between">
-                <LinkPanel border href="/kan-registreres-som-arbeidssoeker" className="mr-8">
-                    <LinkPanel.Title>Arbeidssøkerregistrering</LinkPanel.Title>
-                </LinkPanel>
-                <LinkPanel border href="/kan-registreres-for-sykmeldtoppfoelging">
-                    <LinkPanel.Title>Mer sykmeldtoppfølging</LinkPanel.Title>
-                </LinkPanel>
+                <div>
+                    <List as="ul" title="Velg arbeidssøkerregistrering hvis">
+                        <List.Item>Personen skal være arbeidssøker fra i dag</List.Item>
+                        <List.Item>Personen skal ha oppfølgingsvedtak 14a</List.Item>
+                        <List.Item>Personen skal stå tilmeldt i arbeidssøkerregisteret</List.Item>
+                    </List>
+                    <Button
+                        variant="secondary-neutral"
+                        onClick={() => router.push('/kan-registreres-som-arbeidssoeker')}
+                    >
+                        Start arbeidssøkerregistrering
+                    </Button>
+                </div>
+                <div>
+                    <List as="ul" title="Velg sykmeldtregistrering hvis">
+                        <List.Item>Personen har arbeidsgiver og en aktiv sykemelding</List.Item>
+                        <List.Item>Personen skal ha oppfølging etter at sykefraværet er over</List.Item>
+                        <List.Item>Personen er ikke arbeidssøker</List.Item>
+                    </List>
+                    <Button
+                        variant="secondary-neutral"
+                        onClick={() => router.push('/kan-registreres-for-sykmeldtoppfoelging')}
+                    >
+                        Start sykmeldtregistrering
+                    </Button>
+                </div>
             </div>
         </>
     );
