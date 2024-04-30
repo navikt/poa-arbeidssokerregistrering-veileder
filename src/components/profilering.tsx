653f4b7e4d8bb9581190147702739a2f33fc6e7e
Geir Gåsodden <geir.gasodden@gmail.com>
2024-04-30 07:39:28 +0200

    r oppdaterer visning av profileringen

--- src/components/profilering.tsx
+++ src/components/profilering.tsx
@@ -1,11 +1,22 @@
 import { BodyLong, Box, Heading } from '@navikt/ds-react';
+import { Profilering } from '@navikt/arbeidssokerregisteret-utils';
+
+import { formaterDato } from '../lib/date-utils';
+
+const PROFILERINGSMAP = {
+    ANTATT_GODE_MULIGHETER: 'Standard innsats',
+    ANTATT_BEHOV_FOR_VEILEDNING: 'Situasjonsbestemt innsats',
+    OPPGITT_HINDRINGER: 'Behov for arbeidsevnevurdering',
+    UKJENT_VERDI: 'Ukjent resultat',
+};
 
 interface ProfileringProps {
-    sisteProfilering: any;
+    sisteProfilering: Profilering;
 }
 
-function Profilering(props: ProfileringProps) {
+function Profileringsresultat(props: ProfileringProps) {
     const { sisteProfilering } = props || {};
+    const { profilertTil, sendtInnAv } = sisteProfilering || {};
 
     if (!sisteProfilering) return null;
 
@@ -14,9 +25,10 @@ function Profilering(props: ProfileringProps) {
             <Heading level="1" size="small">
                 Profilering
             </Heading>
-            <BodyLong>{JSON.stringify(sisteProfilering)}</BodyLong>
+            <BodyLong>Resulatet av profileringen er "{PROFILERINGSMAP[profilertTil]}"</BodyLong>
+            <BodyLong spacing>Profileringer er sist oppdatert {formaterDato(sendtInnAv.tidspunkt)}</BodyLong>
         </Box>
     );
 }
 
-export default Profilering;
+export default Profileringsresultat;
