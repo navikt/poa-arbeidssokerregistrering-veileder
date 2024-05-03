28ab3c47795848d0df2cad4734e114fec50d686c
Jostein Holje <jostein.holje@nav.no>
2024-05-03 11:56:50 +0200

    behovsvurdering: PAW_ARBEIDSSOKER_BESVARELSE_SCOPE

--- src/pages/api/behovsvurdering.ts
+++ src/pages/api/behovsvurdering.ts
@@ -1,12 +1,14 @@
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 import { createProxyCall } from '../../lib/proxy-api-kall';
-import { getAiaBackendToken, getHeaders } from '../../lib/next-api-handler';
+import { getPawArbeidssokerBesvarelseToken, getHeaders } from '../../lib/next-api-handler';
 
 const url = `${process.env.AIA_BACKEND_URL}/veileder/behov-for-veiledning`;
 const brukerMock = process.env.ENABLE_MOCK === 'enabled';
 
 export default withAuthenticatedApi(
     createProxyCall(async (req, callId) => {
-        return brukerMock ? getHeaders('token', callId) : getHeaders(await getAiaBackendToken(req), callId);
+        return brukerMock
+            ? getHeaders('token', callId)
+            : getHeaders(await getPawArbeidssokerBesvarelseToken(req), callId);
     }, url),
 );
