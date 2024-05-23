1ffdd14c14e0dba6d79e1e2d4732d8ae5d268282
Geir Gåsodden <geir.gasodden@gmail.com>
2024-05-23 08:24:16 +0200

    r fjerner registreringsmuligheten for de som har aktiv periode

--- src/components/arbeidssoekerstatus-oversikt.tsx
+++ src/components/arbeidssoekerstatus-oversikt.tsx
@@ -12,6 +12,7 @@ import VelgRegistreringsKnapp from './velg-registreringsknapp';
 import ArbeidssoekerperioderOgOpplysningerWrapper from './arbeidssoekerperioder-og-opplysninger-wrapper';
 import VurderingskriterierForArbeidssoekerregistrering from './vurderingskriterier-for-arbeidssoekerregistrering';
 import AarsakerTilAtPersonenIkkeKanRegistreres from './aarsaker-til-at-personen-ikke-kan-registreres';
+import { hentSisteArbeidssokerPeriode } from '../lib/hent-siste-arbeidssoekerperiode';
 
 export const REGLER_SOM_IKKE_KAN_OVERSTYRES = ['IKKE_FUNNET', 'DOED', 'SAVNET', 'ANSATT_IKKE_TILGANG_TIL_BRUKER'];
 
@@ -51,10 +52,17 @@ function ArbeidssoekerstatusOversikt() {
     const [kanStarteArbeidssoekerperiode, setKanStarteArbeidssoekerperiode] = useState<boolean>(false);
     const [error, setError] = useState<any>(undefined);
     const [kanIkkeStarteArbeidssoekerperiode, setKanIkkeStarteArbeidssoekerperiode] = useState<boolean>(false);
+    const [errorArbeidssoekerperioder, setErrorArbeidssoekerperioder] = useState<any>(undefined);
+    const [sisteArbeidssoekerperiode, setSisteArbeidssoekerperiode] = useState<any>({});
+    const [harIkkeAktivPeriode, setHarIkkeAktivPeriode] = useState<boolean>(false);
 
     const sjekkKanStarteArbeidssoekerperiodeUrl = brukerMock
         ? '/api/mocks/kan-starte-arbeidssoekerperiode'
         : '/api/kan-starte-arbeidssoekerperiode';
+
+    const hentArbeidssoekerperioderUrl = brukerMock
+        ? '/api/mocks/oppslag-arbeidssoekerperioder'
+        : '/api/oppslag-arbeidssoekerperioder';
 
     const kanOverstyres = sjekkOmRegelKanOverstyres(error);
 
@@ -85,11 +93,44 @@ function ArbeidssoekerstatusOversikt() {
         }
     }
 
+    async function apiKallArbeidssoekerperioder() {
+        const payload = JSON.stringify({
+            identitetsnummer: fnr,
+        });
+
+        try {
+            const response = await fetch(hentArbeidssoekerperioderUrl, {
+                method: 'POST',
+                body: payload,
+                credentials: 'include',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                const sisteArbeidssoekerperiode = hentSisteArbeidssokerPeriode(data);
+                setSisteArbeidssoekerperiode(sisteArbeidssoekerperiode);
+            }
+        } catch (err: unknown) {
+            setErrorArbeidssoekerperioder(err);
+        }
+    }
+
     useEffect(() => {
         if (fnr && enhetId) {
             apiKall();
+            apiKallArbeidssoekerperioder();
         }
     }, [fnr, enhetId]);
+
+    useEffect(() => {
+        if (sisteArbeidssoekerperiode) {
+            const aktivPeriode =
+                sisteArbeidssoekerperiode?.avsluttet === null && sisteArbeidssoekerperiode !== undefined;
+            setHarIkkeAktivPeriode(!aktivPeriode);
+        }
+    }, [sisteArbeidssoekerperiode]);
 
     if (!fnr || !enhetId) return null;
 
@@ -104,8 +145,13 @@ function ArbeidssoekerstatusOversikt() {
             </Heading>
             <AarsakerTilAtPersonenIkkeKanRegistreres feilmelding={error} />
             <VurderingskriterierForArbeidssoekerregistrering feilmelding={error} />
-            {(kanOverstyres || kanStarteArbeidssoekerperiode) && <GenereltOmSamtykke />}
-            <VelgRegistreringsKnapp feilmelding={error} kanStarteArbeidssoekerperiode={kanStarteArbeidssoekerperiode} />
+            {(kanOverstyres || kanStarteArbeidssoekerperiode) && harIkkeAktivPeriode && <GenereltOmSamtykke />}
+            {harIkkeAktivPeriode && (
+                <VelgRegistreringsKnapp
+                    feilmelding={error}
+                    kanStarteArbeidssoekerperiode={kanStarteArbeidssoekerperiode}
+                />
+            )}
             <div className="mt-8">
                 <ArbeidssoekerperioderOgOpplysningerWrapper />
             </div>
