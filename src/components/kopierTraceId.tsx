1ec2422993e6961920974afba9fd4a53b7bbf041
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-05-23 08:43:37 +0200

    laget komponent for å vise traceID til vieledere

---
+++ src/components/kopierTraceId.tsx
@@ -0,0 +1,19 @@
+import { Box, CopyButton } from '@navikt/ds-react';
+
+interface TraceIdProps {
+    traceId: string;
+}
+
+const KopierTraceId = (props: TraceIdProps) => {
+    const { traceId } = props;
+
+    return (
+        <Box className="mt-4">
+            Dersom du kontakter brukerstøtte blir det lettere for oss å hjelpe deg hvis du legger ved sporingsID-en
+            under. Du kopierer den ved å trykke på knappen.
+            <CopyButton copyText="3.14" text={traceId} activeText="Kopierte traceId" />
+        </Box>
+    );
+};
+
+export default KopierTraceId;
