70784feaf24eacbc27588e2dff6d180d35111361
Jostein Holje <jostein.holje@nav.no>
2024-09-18 11:36:57 +0200

    useMockData

--- src/pages/api/tilgjengelige-bekreftelser.ts
+++ src/pages/api/tilgjengelige-bekreftelser.ts
@@ -1,6 +1,6 @@
 import lagApiHandlerMedAuthHeaders, { getBekreftelseAPIToken } from '../../lib/next-api-handler';
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 
-const url = `${process.env.BEKREFTELSE_API_URL}/api/v1/tilgjengelige-bekreftelser`;
+const url = `${process.env.BEKREFTELSE_API_URL}/api/v1/tilgjengelige-bekreftelser?useMockData=true`;
 
 export default withAuthenticatedApi(lagApiHandlerMedAuthHeaders(url, getBekreftelseAPIToken, { method: 'POST' }));
