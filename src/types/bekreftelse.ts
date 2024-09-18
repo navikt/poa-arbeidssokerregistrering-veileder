eff4b26eba3e545a028fd3cc522e6e85abcdfca5
Jostein Holje <jostein.holje@nav.no>
2024-09-18 12:47:55 +0200

    Oppdaterte typer på bekreftelse

--- src/types/bekreftelse.ts
+++ src/types/bekreftelse.ts
@@ -1,13 +1,17 @@
 export interface Bekreftelse {
-    harVaertIArbeid: boolean;
-    oenskerAaVaereRegistrert: boolean;
+    harJobbetIDennePerioden: boolean;
+    vilFortsetteSomArbeidssoeker: boolean;
     bekreftelseId: string;
 }
-
 export interface TilgjengeligBekreftelse {
+    periodeId: string;
     bekreftelseId: string;
     gjelderFra: string;
     gjelderTil: string;
 }
-
 export type TilgjengeligeBekreftelser = TilgjengeligBekreftelse[];
+export interface SistInnsendteBekreftelse {
+    harJobbetIDennePerioden: boolean;
+    vilFortsetteSomArbeidssoeker: boolean;
+    dato: string;
+}
