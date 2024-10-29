3f77842f5e507d9193781cf372fbb3511aa47203
Geir Gåsodden <geir.gasodden@gmail.com>
2024-10-29 08:57:28 +0100

    r oppdaterer ikke params ved endringer i dekoratøren

--- src/components/InternflateDecorator.tsx
+++ src/components/InternflateDecorator.tsx
@@ -1,5 +1,6 @@
 import NAVSPA from '@navikt/navspa';
 import { ComponentType } from 'react';
+import { useRouter } from 'next/router';
 
 import { useParamsFromContext } from '../contexts/params-from-context';
 import { DecoratorProps, Environment, UrlFormat } from '../model/internflate-decorator';
@@ -8,18 +9,27 @@ import { Config } from '../model/config';
 
 const Decorator: ComponentType<DecoratorProps> = NAVSPA.importer('internarbeidsflate-decorator-v3');
 
+const routesSomIkkeSkalRefreshes = ['/avslutt-arbeidssoekerperiode', '/sykmeldtoppfoelging'];
+
 const InternflateDecorator = () => {
-    const { params, setParams } = useParamsFromContext();
+    const { params } = useParamsFromContext();
     const { fnr, enhetId } = params;
     const { enableMock, decoratorEnv } = useConfig() as Config;
+    const router = useRouter();
     const brukerMock = typeof enableMock === 'undefined' || enableMock === 'enabled';
 
     const onFnrChanged = (fnr) => {
-        setParams({ fnr: fnr });
+        // setParams({ fnr: fnr });
+        if (!routesSomIkkeSkalRefreshes.includes(router.pathname)) {
+            router.push('/');
+        }
     };
 
     const onEnhetChanged = (enhet) => {
-        setParams({ enhetId: enhet });
+        // setParams({ enhetId: enhet });
+        if (!routesSomIkkeSkalRefreshes.includes(router.pathname)) {
+            router.push('/');
+        }
     };
 
     const props = {
