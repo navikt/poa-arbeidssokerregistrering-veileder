d8b898ecf4ebfbd254c775cb11686168d68f100e
Geir Gåsodden <geir.gasodden@gmail.com>
2025-01-14 07:44:02 +0100

    r sjekker lengden på arbeidssøkerperioderarray

--- src/components/arbeidssoekerperioder-og-opplysninger-wrapper.tsx
+++ src/components/arbeidssoekerperioder-og-opplysninger-wrapper.tsx
@@ -102,7 +102,7 @@ function ArbeidssoekerperioderOgOpplysningerWrapper() {
     const sisteProfilering = hentSisteProfilering(samletInformasjon?.profilering ?? []);
     const aktivPeriode = sisteArbeidssoekerperiode?.avsluttet === null && sisteArbeidssoekerperiode !== undefined;
     const harOpplysninger = sisteOpplysningerOmArbeidssoeker?.opplysningerOmArbeidssoekerId;
-    const harIngenArbeidssoekerperioder = !samletInformasjon?.arbeidssoekerperioder;
+    const harIngenArbeidssoekerperioder = samletInformasjon?.arbeidssoekerperioder.length === 0;
 
     return (
         <>
