6fbcceff40f4a1ca6b181bbfcfd248dcbe2b49c7
Jostein Holje <jostein.holje@nav.no>
2025-01-14 12:58:52 +0100

    Send aia-backend scopet token til aia-backend/veileder/behov-for-veiledning

--- src/pages/api/behovsvurdering.ts
+++ src/pages/api/behovsvurdering.ts
@@ -1,14 +1,12 @@
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 import { createProxyCall } from '../../lib/proxy-api-kall';
-import { getPawArbeidssokerBesvarelseToken, getHeaders } from '../../lib/next-api-handler';
+import { getPawArbeidssokerBesvarelseToken, getHeaders, getAiaBackendAzureToken } from '../../lib/next-api-handler';
 
 const url = `${process.env.AIA_BACKEND_URL}/veileder/behov-for-veiledning`;
 const brukerMock = process.env.ENABLE_MOCK === 'enabled';
 
 export default withAuthenticatedApi(
     createProxyCall(async (req, callId) => {
-        return brukerMock
-            ? getHeaders('token', callId)
-            : getHeaders(await getPawArbeidssokerBesvarelseToken(req), callId);
+        return brukerMock ? getHeaders('token', callId) : getHeaders(await getAiaBackendAzureToken(req), callId);
     }, url),
 );
