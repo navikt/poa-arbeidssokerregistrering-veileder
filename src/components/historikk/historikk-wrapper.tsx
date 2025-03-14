5dbd6c0b4180a7f373b1d01294c71b5a0544dfc5
Geir Gåsodden <geir.gasodden@gmail.com>
2025-03-14 07:02:14 +0100

    r oppdaterer oversettelser av sluttårsak

--- src/components/historikk/historikk-wrapper.tsx
+++ src/components/historikk/historikk-wrapper.tsx
@@ -22,9 +22,21 @@ const TEKSTER = {
         VEILEDER: 'veileder',
         'fortsatt aktiv': 'fortsatt aktiv',
         'graceperiode utløpt': 'Ikke bekreftet arbeidssøkerstatus',
-        'stopp av periode': 'Stoppet av veileder',
+        'stopp av periode': 'Arbeidssøkerperioden er avsluttet av veileder',
         feilregistrering: 'Slettet på grunn av feilregistrering',
         "svarte nei på spørsmål 'vil du fortsatt være registrert som arbeidssøker?'": 'Stoppet av bruker',
+        'personen er ikke bosatt etter folkeregisterloven':
+            'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
+        'avvist fordi personen ikke er bosatt i Norge i henhold til folkeregisterloven':
+            'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
+        'er registrert som død, avvist fordi personen ikke er bosatt i Norge i henhold til folkeregisterloven':
+            'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
+        'personen er doed': 'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
+        'er registrert som død': 'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
+        iserv: 'Arbeidssøkerperioden er avsluttet i Arena',
+        overføring: 'Arbeidssøkerperioden er avsluttet i Arena',
+        'har ugyldig/annullert identitet, kunne ikke fastslå alder, avvist fordi personen ikke er bosatt i Norge i henhold til folkeregisterloven':
+            'Personen oppfyller ikke lenger kravene til å være registrert arbeidssøker',
     },
 };
 
