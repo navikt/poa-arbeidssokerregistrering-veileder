a1f55b3a118f750215230e90aebc747cde84c362
Geir Gåsodden <geir.gasodden@gmail.com>
2025-06-05 07:30:04 +0200

    r slår opp gyldige bekreftelser og merger de med aggregerte perioder

--- src/pages/historikk.tsx
+++ src/pages/historikk.tsx
@@ -1,28 +1,50 @@
+import { Alert, Heading, Loader } from '@navikt/ds-react';
+
 import { useParamsFromContext } from '../contexts/params-from-context';
 import { useConfig } from '../contexts/config-context';
-import { Config } from '../model/config';
 import useApiKall from '../hooks/useApiKall';
-import { TilgjengeligeBekreftelser } from '../types/bekreftelse';
+import { Config } from '../model/config';
 import TilbakeTilForside from '../components/tilbake-til-forside';
 import { withAuthenticatedPage } from '../auth/withAuthentication';
-import { Alert, Heading, Loader } from '@navikt/ds-react';
 import { HistorikkWrapper } from '../components/historikk/historikk-wrapper';
 import { AggregertePerioder, AggregertPeriode } from '../types/aggregerte-perioder';
+import { AggregerteBekreftelser } from '../model/bekreftelse';
+import { mergeGyldigeBekreftelser } from '../lib/merge-gyldige-bekreftelser';
 
 export default function Historikk() {
     const { params } = useParamsFromContext();
     const { enableMock } = useConfig() as Config;
     const { fnr } = params;
     const brukerMock = enableMock === 'enabled';
+
     const {
         data: aggregertePerioder,
-        isLoading,
-        error,
+        isLoading: isLoadingAggregertePerioder,
+        error: errorAggregertePerioder,
     } = useApiKall<AggregertePerioder>(
         `/api/${brukerMock ? 'mocks/' : ''}oppslag-arbeidssoekerperioder-aggregert`,
         'POST',
         fnr ? JSON.stringify({ identitetsnummer: fnr }) : null,
     );
+
+    const periodeIds = aggregertePerioder.map((periode) => periode.periodeId);
+
+    const {
+        data: gyldigeBekreftelser,
+        isLoading: isLoadingGyldigeBekreftelser,
+        error: errorGyldigeBekreftelser,
+    } = useApiKall<AggregerteBekreftelser>(
+        `/api/${brukerMock ? 'mocks/' : ''}gyldige-bekreftelser`,
+        'POST',
+        fnr ? JSON.stringify({ identitetsnummer: fnr, periodeIds: periodeIds }) : null,
+    );
+
+    const isLoading = isLoadingAggregertePerioder || isLoadingGyldigeBekreftelser;
+    const error = errorAggregertePerioder || errorGyldigeBekreftelser;
+    const aggregertePerioderMedGyldigeBekreftelser =
+        aggregertePerioder && gyldigeBekreftelser
+            ? mergeGyldigeBekreftelser(aggregertePerioder, gyldigeBekreftelser)
+            : null;
 
     if (isLoading) {
         return (
@@ -41,8 +63,8 @@ export default function Historikk() {
             <TilbakeTilForside sidenavn="Arbeidssøkerhistorikk" />
             <Heading size={'large'}>Arbeidssøkerhistorikk</Heading>
             <div className={'flex flex-col max-w-3xl'}>
-                {aggregertePerioder &&
-                    aggregertePerioder.map((periode, index) => (
+                {aggregertePerioderMedGyldigeBekreftelser &&
+                    aggregertePerioderMedGyldigeBekreftelser.map((periode, index) => (
                         <div
                             className={'p-4'}
                             key={periode.periodeId}
