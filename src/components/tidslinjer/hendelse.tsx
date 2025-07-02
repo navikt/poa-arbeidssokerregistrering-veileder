4606cbea3a5ceb3b2ce0c1989a8c517021bf1ecd
Geir Gåsodden <geir.gasodden@gmail.com>
2025-07-02 09:33:43 +0200

    r plukker ut relevanet data fra hver hendelse

--- src/components/tidslinjer/hendelse.tsx
+++ src/components/tidslinjer/hendelse.tsx
@@ -4,12 +4,27 @@ import { prettyPrintDatoOgKlokkeslett } from '../../lib/date-utils';
 
 import { Hendelse } from '../../model/tidslinjer';
 
+function snakeToCamel(snakeCaseString) {
+    return snakeCaseString.replace(/_([a-z])/g, (match, char) => char.toUpperCase());
+}
+
+function hentUtfoertAv(data) {
+    const utfoerer = data.bekreftelse
+        ? data.bekreftelse.svar.sendtInnAv.utfoertAv
+        : data.sendtInnAv
+          ? data.sendtInnAv.utfoertAv
+          : data.utfoertAv;
+    return utfoerer.type;
+}
+
 export function HendelseVisning(props: Hendelse) {
     const { hendelseType, tidspunkt } = props;
+    const data = props[snakeToCamel(hendelseType)];
 
     return (
         <Box>
-            {hendelseType} - {prettyPrintDatoOgKlokkeslett(tidspunkt)}
+            {hendelseType} - {prettyPrintDatoOgKlokkeslett(tidspunkt, 'nb', true)} - {hentUtfoertAv(data)} -{' '}
+            {data.aarsak ? data.aarsak : ''}
         </Box>
     );
 }
--- src/model/tidslinjer.ts
+++ src/model/tidslinjer.ts
@@ -1,8 +1,30 @@
-export type Hendelse = {
+import { Profilering, OpplysningerOmArbeidssoker, UtfoertAv, Bekreftelse } from '@navikt/arbeidssokerregisteret-utils';
+import { BekreftelseStatus } from './bekreftelse';
+
+export type PeriodeStart = {
+    tidspunkt: string;
+    utfoertAv: UtfoertAv;
+    kilde: string;
+    aarsak: string;
+};
+
+export type PeriodeStopp = {
+    tidspunkt: string;
+    utfoertAv: UtfoertAv;
+    kilde: string;
+    aarsak: string;
+};
+
+export type BekreftelseHendelse = {
+    status: BekreftelseStatus;
+    bekreftelse: Bekreftelse;
+};
+
+export interface Hendelse {
     hendelseType: string;
     tidspunkt: string;
-    [index: string]: any;
-};
+    [index: string]: PeriodeStart | PeriodeStopp | OpplysningerOmArbeidssoker | Profilering | BekreftelseHendelse | any;
+}
 
 export type Tidslinje = {
     periodeId: string;
