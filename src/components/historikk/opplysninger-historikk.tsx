cf88e021fdd68ef1a3b9a6b9c3e5625a32485e3d
Geir Gåsodden <geir.gasodden@gmail.com>
2025-07-08 08:28:24 +0200

    b kommenterer ut egenvurdering enn så lenge

--- src/components/historikk/opplysninger-historikk.tsx
+++ src/components/historikk/opplysninger-historikk.tsx
@@ -6,7 +6,7 @@ import { OpplysningerMedProfilering } from '../../types/aggregerte-perioder';
 import { prettyPrintDato, prettyPrintDatoOgKlokkeslett } from '../../lib/date-utils';
 import { OpplysningerKomponent } from '../opplysninger-om-arbeidssoker-komponent';
 import ProfileringKomponent from './profilering';
-import EgenvurderingerKomponent from './egenvurderinger';
+//import EgenvurderingerKomponent from './egenvurderinger';
 
 const TEKSTER = {
     nb: {
@@ -54,7 +54,7 @@ export function OpplysningerHistorikk(props: Props) {
                                 </Box>
                                 <OpplysningerKomponent opplysninger={opplysninger} />
                                 <ProfileringKomponent profilering={opplysninger.profilering} />
-                                <EgenvurderingerKomponent egenvurderinger={opplysninger.profilering.egenvurderinger} />
+                                {/*<EgenvurderingerKomponent egenvurderinger={opplysninger.profilering?.egenvurderinger} />*/}
                             </Accordion.Content>
                         </Accordion.Item>
                     );
