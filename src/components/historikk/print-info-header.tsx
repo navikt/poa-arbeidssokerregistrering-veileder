ed6b33f4421a5f801c5dc853e79b6fa3b2def08a
Jostein Holje <jostein.holje@nav.no>
2025-07-24 13:58:27 +0200

    prettyprint dato med år

--- src/components/historikk/print-info-header.tsx
+++ src/components/historikk/print-info-header.tsx
@@ -11,7 +11,9 @@ const PrintInfoHeader = (props: Props) => {
             <div className={'flex flex-col content-center justify-center flex-wrap'}>
                 <Image src={'/nav-logo.svg'} alt={'Nav Logo'} width={100} height={50} />
             </div>
-            <div className={'mt-8 mb-4'}>Utskriftsdato: {prettyPrintDatoOgKlokkeslett(new Date().toISOString())}</div>
+            <div className={'mt-8 mb-4'}>
+                Utskriftsdato: {prettyPrintDatoOgKlokkeslett(new Date().toISOString(), 'nb', true)}
+            </div>
             <div>Fødselsnummer: {props.fnr}</div>
         </section>
     );
