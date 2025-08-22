0e6ead242ffac84c083326a1eee52a9fd176baf3
Geir Gåsodden <geir.gasodden@gmail.com>
2025-08-22 12:24:55 +0200

    c peker om aggregerte opplysninger til å gå mot v2

--- src/pages/api/oppslag-arbeidssoekerperioder-aggregert.ts
+++ src/pages/api/oppslag-arbeidssoekerperioder-aggregert.ts
@@ -1,6 +1,6 @@
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
 import lagOppslagsApiKall from '../../lib/lag-oppslagsapi-kall';
 
-const url = `${process.env.OPPSLAG_API_URL}/api/v1/veileder/arbeidssoekerperioder-aggregert`;
+const url = `${process.env.OPPSLAG_API_V2_URL}/api/v1/veileder/arbeidssoekerperioder-aggregert`;
 
 export default withAuthenticatedApi(lagOppslagsApiKall(url, { method: 'POST' }));
