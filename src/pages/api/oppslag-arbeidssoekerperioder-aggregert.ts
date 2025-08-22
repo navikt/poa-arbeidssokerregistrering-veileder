6f639be03f66a17547b882316cf4b32c751c1473
Geir Gåsodden <geir.gasodden@gmail.com>
2025-08-22 12:39:13 +0200

    c oppdaterer til lagOppslagsApiV2Kall

--- src/pages/api/oppslag-arbeidssoekerperioder-aggregert.ts
+++ src/pages/api/oppslag-arbeidssoekerperioder-aggregert.ts
@@ -1,6 +1,6 @@
 import { withAuthenticatedApi } from '../../auth/withAuthentication';
-import lagOppslagsApiKall from '../../lib/lag-oppslagsapi-kall';
+import lagOppslagsApiV2Kall from '../../lib/lag-oppslagsapi-v2-kall';
 
 const url = `${process.env.OPPSLAG_API_V2_URL}/api/v1/veileder/arbeidssoekerperioder-aggregert`;
 
-export default withAuthenticatedApi(lagOppslagsApiKall(url, { method: 'POST' }));
+export default withAuthenticatedApi(lagOppslagsApiV2Kall(url, { method: 'POST' }));
