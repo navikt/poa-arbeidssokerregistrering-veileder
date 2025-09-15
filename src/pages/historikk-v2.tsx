6d63db2c3da09a10118b2a366518832791a43c0e
Christian Rennemo <christian.rennemo@nav.no>
2025-09-15 14:55:52 +0200

    added loading state

--- src/pages/historikk-v2.tsx
+++ src/pages/historikk-v2.tsx
@@ -5,12 +5,17 @@ import { AggregertePerioder } from '@navikt/arbeidssokerregisteret-utils';
 import { useParamsFromContext } from '../contexts/params-from-context';
 import { TidslinjeSelectionProvider, useTidslinjeSelection } from '../contexts/tidslinje-selection-context';
 import { Historikk } from '../components/historikk-v2/historikk';
-import { ActionMenu, Alert, BodyShort, Box, Button, Heading } from '@navikt/ds-react';
+import { ActionMenu, Alert, BodyShort, Box, Button, Heading, Skeleton } from '@navikt/ds-react';
 import { HistorikkListeTittel } from '../components/historikk-v2/historikk-liste-tittel';
 import { ChevronDownIcon } from '@navikt/aksel-icons';
 import { ApiTidslinjeResponse, Tidslinje } from '../model/schema-api.types';
 
-const HistorikkInnhold = ({ tidslinjeResponse }: { tidslinjeResponse: ApiTidslinjeResponse | undefined }) => {
+type HistorikkInnholdProps = {
+    tidslinjeResponse: ApiTidslinjeResponse | undefined;
+    isLoading: boolean;
+};
+
+const HistorikkInnhold = ({ tidslinjeResponse, isLoading }: HistorikkInnholdProps) => {
     const { selectedTidslinje } = useTidslinjeSelection();
     if (!tidslinjeResponse) {
         return <div>Ingen data tilgjengelig</div>;
@@ -19,6 +24,35 @@ const HistorikkInnhold = ({ tidslinjeResponse }: { tidslinjeResponse: ApiTidslin
 
     if (!tidslinjeList || tidslinjeList.length === 0) {
         return <div>Ingen data tilgjengelig</div>;
+    }
+
+    // TODO: Vi kan lage loading state her, med skeleton
+    if (isLoading) {
+        return (
+            <div className="flex-1 gap-4 md:grid md:grid-cols-[minmax(300px,1fr)_3fr]">
+                <Box className="hidden md:block">
+                    <Heading size="large">Arbeidssøkerperioder</Heading>
+                    <Skeleton variant="rectangle" height={30} className="mb-4" />
+                    {Array(5)
+                        .fill(0)
+                        .map((_, i) => (
+                            <Box key={i}>
+                                <Skeleton variant="rectangle" height={60} className="mb-2" />
+                            </Box>
+                        ))}
+                </Box>
+                <Box>
+                    <Skeleton variant="rounded" height={250} className="mb-8" />
+                    {Array(5)
+                        .fill(0)
+                        .map((_, i) => (
+                            <Box key={i}>
+                                <Skeleton variant="rounded" height={60} className="mb-2" />
+                            </Box>
+                        ))}
+                </Box>
+            </div>
+        );
     }
 
     return (
@@ -97,7 +131,7 @@ const HistorikkTidslinjer = () => {
 
     return (
         <TidslinjeSelectionProvider>
-            <HistorikkInnhold tidslinjeResponse={tidslinjerResponse} />
+            <HistorikkInnhold tidslinjeResponse={tidslinjerResponse} isLoading={isLoadingTidslinjer} />
         </TidslinjeSelectionProvider>
     );
 };
