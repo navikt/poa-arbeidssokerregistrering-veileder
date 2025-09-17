90a205cbe2a69fe2fabf8d0f734717c6c0609e6d
Christian Rennemo <christian.rennemo@nav.no>
2025-09-17 15:21:49 +0200

    glemte slash

--- src/contexts/params-from-context.tsx
+++ src/contexts/params-from-context.tsx
@@ -24,7 +24,7 @@ const routesSomIkkeSkalRefreshes = [
     '/tidslinjer',
     '/registrering-arbeidssoeker-sjekk',
     '/oppdater-opplysninger',
-    'historikk-v2',
+    '/historikk-v2',
 ];
 
 function ParamsFromContextProvider({ children }) {
