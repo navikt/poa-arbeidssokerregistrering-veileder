2b3dd7d91fbdaa240d9b17c49a0a4b0d38777fa3
Christian Rennemo <christian.rennemo@nav.no>
2025-09-22 15:22:53 +0200

    kopiert over mapping fra gammel historikk, men støtter ny type, inkludert din situasjon som er ulik i tidslinjer vs gammel historikk

---
+++ src/components/historikk-v2/hendelseTyper/map-opplysninger-til-fremvisning.ts
@@ -0,0 +1,55 @@
+import { mapNusKodeTilUtdannignsnivaa, SporsmalId, Svar } from '@navikt/arbeidssokerregisteret-utils';
+import { OpplysningerOmArbeidssoeker } from '../../../model/schema-api.types';
+
+type OpplysningProps = { sporsmal: string; svar: Svar | string };
+
+function getDinSituasjonSvar(opplysninger: OpplysningerOmArbeidssoeker) {
+    const situasjon = opplysninger.jobbsituasjon.beskrivelser[0];
+    return situasjon.beskrivelse || 'Ikke oppgitt';
+}
+function getSisteStillingSvar(opplysninger: OpplysningerOmArbeidssoeker) {
+    const detaljer = opplysninger.jobbsituasjon[0]?.detaljer;
+    return detaljer?.stilling || 'Ikke oppgitt';
+}
+
+/**
+ * Mapper om data fra tidslinje-api til et format som kan brukes i UI.
+ *
+ * @param opplysninger - Data fra Tidslinje-api'et
+ * @returns En liste med objekter, hver med en spørsmål-identifikator og det tilhørende svaret,
+ *          egnet for visning i frontend.
+ */
+export function mapOpplysningerV2(opplysninger: OpplysningerOmArbeidssoeker): OpplysningProps[] {
+    const result: OpplysningProps[] = [
+        {
+            sporsmal: SporsmalId.dinSituasjon,
+            svar: getDinSituasjonSvar(opplysninger),
+        },
+        {
+            sporsmal: SporsmalId.utdanning,
+            svar: mapNusKodeTilUtdannignsnivaa(opplysninger.utdanning.nus),
+        },
+        {
+            sporsmal: SporsmalId.utdanningBestatt,
+            svar: opplysninger.utdanning.bestaatt,
+        },
+        {
+            sporsmal: SporsmalId.andreForhold,
+            svar: opplysninger.annet?.andreForholdHindrerArbeid || 'Mangler opplysninger',
+        },
+        {
+            sporsmal: SporsmalId.sisteStilling,
+            svar: getSisteStillingSvar(opplysninger),
+        },
+        {
+            sporsmal: SporsmalId.utdanningGodkjent,
+            svar: opplysninger.utdanning.godkjent,
+        },
+        {
+            sporsmal: SporsmalId.helseHinder,
+            svar: opplysninger.helse?.helsetilstandHindrerArbeid || 'Mangler opplysninger',
+        },
+    ];
+
+    return result;
+}
