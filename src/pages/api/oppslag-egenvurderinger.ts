5852eae3ab4de3679e72732a135c65fb0a4e1513
Jostein Holje <jostein.holje@nav.no>
2025-09-25 10:51:23 +0200

    Slett ubrukt api/oppslag-egenvurderinger

--- src/pages/api/oppslag-egenvurderinger.ts
+++
@@ -1,6 +0,0 @@
-import { withAuthenticatedApi } from '../../auth/withAuthentication';
-import lagOppslagsApiKall from '../../lib/lag-oppslagsapi-kall';
-
-const url = `${process.env.OPPSLAG_API_URL}/api/v1/veileder/profilering/egenvurderinger`;
-
-export default withAuthenticatedApi(lagOppslagsApiKall(url, { method: 'POST' }));
