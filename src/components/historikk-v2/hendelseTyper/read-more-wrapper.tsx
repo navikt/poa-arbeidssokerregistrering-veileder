f71f441cb44280018334426649d457a8f320050d
Christian Rennemo <christian.rennemo@nav.no>
2025-10-08 08:44:30 +0200

    trekk ut repeterende kode til en wrapper

---
+++ src/components/historikk-v2/hendelseTyper/read-more-wrapper.tsx
@@ -0,0 +1,32 @@
+import { ReadMore } from '@navikt/ds-react';
+import React, { useEffect, useState } from 'react';
+import { useVisningTypeContext } from '../../../contexts/hendelse-visning-context';
+
+type ReadMoreWrapperProps = {
+    header: string;
+    children: React.ReactNode;
+};
+
+/**
+ * ReadMoreWrapper
+ *
+ * En tynn wrapper rundt @navikt/ds-react sin ReadMore som synkroniserer
+ * lokal "open"-tilstand mot global visningsmodus fra hendelse-visning-context.
+ */
+const ReadMoreWrapper: React.FC<ReadMoreWrapperProps> = (props) => {
+    const { header, children, ...rest } = props;
+    const [open, setOpen] = useState(false);
+    const { visningsType } = useVisningTypeContext();
+
+    useEffect(() => {
+        setOpen(visningsType === 'expanded');
+    }, [visningsType]);
+
+    return (
+        <ReadMore header={header} onOpenChange={setOpen} open={open} {...rest}>
+            {children}
+        </ReadMore>
+    );
+};
+
+export { ReadMoreWrapper };
