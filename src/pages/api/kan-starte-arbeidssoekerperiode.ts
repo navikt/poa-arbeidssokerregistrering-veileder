264d3d1d20aeb8c49f9f952900079d427183bc88
Jostein Holje <jostein.holje@nav.no>
2025-12-19 12:20:29 +0100

    slett ubrukte api-filer

--- src/pages/api/kan-starte-arbeidssoekerperiode.ts
+++
@@ -1,6 +0,0 @@
-import { withAuthenticatedApi } from '../../auth/withAuthentication';
-import lagArbeidssokerApiKall from '../../lib/lag-arbeidssoker-api-kall';
-
-const url = `${process.env.INNGANG_API_URL}/api/v1/arbeidssoker/kanStartePeriode`;
-
-export default withAuthenticatedApi(lagArbeidssokerApiKall(url, { method: 'PUT' }));
