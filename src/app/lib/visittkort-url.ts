58fb4d17e2296731265dfe337a990cc1823f27a4
Christian Rennemo <christian.rennemo@nav.no>
2026-02-05 13:50:03 +0100

    legg til logging - debugger

--- src/app/lib/visittkort-url.ts
+++ src/app/lib/visittkort-url.ts
@@ -10,7 +10,9 @@ async function hentVisittkortScriptUrl(): Promise<string | null> {
     const cdnUrl = `https://cdn.nav.no/poao/veilarbvisittkortfs-${isProduction ? 'prod' : 'dev'}/build`;
     try {
         const res = await fetch(`${cdnUrl}/asset-manifest.json`);
+        logger.info('fetch manifest');
         const manifest: Manifest = await res.json();
+        logger.info(`${JSON.stringify(manifest)}`);
         return `${cdnUrl}/${manifest['index.html'].file}`;
     } catch (e) {
         logger.error(`Klarte ikke å hente visittkort-url. ${e}`);
