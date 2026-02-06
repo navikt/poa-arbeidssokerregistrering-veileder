cc72e7304d6fb0c732190ee6de1f7e81ec2295fd
Christian Rennemo <christian.rennemo@nav.no>
2026-02-06 08:46:06 +0100

    lagt til biome, kjører kun på app-router

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 const eslintConfig = [
     ...nextVitals,
     {
-        ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+        ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'src/app/**'],
     },
 ];
 
