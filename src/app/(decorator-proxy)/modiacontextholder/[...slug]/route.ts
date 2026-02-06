5ee0f882ff3ee1f89fe21f009fc452462439fc73
Christian Rennemo <christian.rennemo@nav.no>
2026-02-06 12:30:35 +0100

    første route som bruker ny proxy kall funksjon

---
+++ src/app/(decorator-proxy)/modiacontextholder/[...slug]/route.ts
@@ -0,0 +1,11 @@
+import { lagProxyKall } from '../../_lib/proxy-handler';
+
+const handler = lagProxyKall({
+	baseUrl: `${process.env.MODIACONTEXTHOLDER_URL}/api`,
+	scope: `api://${process.env.MODIACONTEXTHOLDER_AAD_APP_CLIENT_ID}/.default`,
+});
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const DELETE = handler;
