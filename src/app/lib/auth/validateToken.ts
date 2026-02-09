d6e3fef73ff6486d71e7bffd17e1286c5c952eea
Christian Rennemo <christian.rennemo@nav.no>
2026-02-09 15:47:17 +0100

    bruk type fra oasis ved håndtering

--- src/app/lib/auth/validateToken.ts
+++ src/app/lib/auth/validateToken.ts
@@ -3,22 +3,39 @@ import { validateAzureToken } from '@navikt/oasis';
 
 /**
  * Validation of Azure token
- * !TODO: Should we return simple bool, or custom object with msg?
  */
 const brukerMock = process.env.ENABLE_MOCK === 'enabled';
+type TokenValidationSuccess = { ok: true };
+type TokenValidationFailure = {
+	ok: false;
+	errorType: 'missing-token' | 'token expired' | 'unknown';
+	error: Error;
+};
 
-async function validateToken(bearerToken: string): Promise<{
-	ok: boolean;
-}> {
+export type TokenValidationResult = TokenValidationSuccess | TokenValidationFailure;
+
+async function validateToken(bearerToken: string): Promise<TokenValidationResult> {
 	if (brukerMock) {
-		return { ok: true };
+		return {
+			ok: true,
+		};
 	}
 
-	if (!bearerToken) return { ok: false };
+	if (!bearerToken) {
+		return {
+			ok: false,
+			errorType: 'missing-token',
+			error: new Error('Bearer token is missing'),
+		};
+	}
 
-	const validated = await validateAzureToken(bearerToken);
-	if (!validated.ok) {
-		return { ok: false };
+	const result = await validateAzureToken(bearerToken);
+	if (result.ok === false) {
+		return {
+			ok: false,
+			errorType: result.errorType,
+			error: result.error,
+		};
 	}
 	return { ok: true };
 }
