7ef38b71dbf94a3bf9f7a000a2d3ee8d4da5d4d1
Christian Rennemo <christian.rennemo@nav.no>
2026-02-11 15:47:50 +0100

    ny variant av getOboToken funksjon

---
+++ src/app/lib/auth/oboToken.ts
@@ -0,0 +1,57 @@
+import 'server-only';
+import { logger } from '@navikt/next-logger';
+import { getToken, requestAzureOboToken, validateAzureToken } from '@navikt/oasis';
+
+const brukerMock = process.env.ENABLE_MOCK === 'enabled';
+
+type OboTokenSuccess = { ok: true; token: string };
+type OboTokenFailure = { ok: false; error: Error };
+type OboTokenResult = OboTokenSuccess | OboTokenFailure;
+
+async function getOboTokenFromRequest(headerList: Headers, scope: string): Promise<OboTokenResult> {
+	if (brukerMock) {
+		return {
+			ok: true,
+			token: 'mocked-token',
+		};
+	}
+	if (!headerList) {
+		return {
+			ok: false,
+			error: new Error('Ingen token funnet'),
+		};
+	}
+
+	const incommingToken = getToken(headerList);
+	if (!incommingToken) {
+		return {
+			ok: false,
+			error: new Error('Ingen token funnet'),
+		};
+	}
+
+	const validation = await validateAzureToken(incommingToken);
+	if (!validation.ok) {
+		logger.info('Ugyldig token');
+		return {
+			ok: false,
+			error: new Error('Ugyldig token'),
+		};
+	}
+
+	const result = await requestAzureOboToken(incommingToken, scope);
+	if (!result.ok) {
+		logger.error(`Tokenutveksling feilet for ${scope}`);
+		return {
+			ok: false,
+			error: new Error(`Tokenutveksling feilet for OBO med følgende scope: ${scope}`),
+		};
+	}
+	return {
+		ok: true,
+		token: result.token,
+	};
+}
+
+export { getOboTokenFromRequest };
+export type { OboTokenResult };
