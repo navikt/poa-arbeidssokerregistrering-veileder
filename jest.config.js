a9d14a3b09ceb86347807c6256a25695403327d1
Christian Rennemo <christian.rennemo@nav.no>
2026-02-12 14:36:50 +0100

    lagt til vitest

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['<rootDir>/src/app/'],
     // resolver: '<rootDir>/test/resolver.js', // temp workaround - uuid feiler for jest 28
 };
 
