bc62d4a2304d887e592f80768540c6729010b98e
Christian Rennemo <christian.rennemo@nav.no>
2026-02-16 13:01:47 +0100

    slett alle de gamle eksterne api proxyene

--- src/pages/api/modiacontextholder/[...slug].ts
+++
@@ -1,5 +0,0 @@
-import { withAuthenticatedApi } from '../../../auth/withAuthentication';
-import { lagModiaContextKall } from '../hent-modia-context';
-
-const url = `${process.env.MODIACONTEXTHOLDER_URL}/api/[slug]`;
-export default withAuthenticatedApi(lagModiaContextKall(url));
