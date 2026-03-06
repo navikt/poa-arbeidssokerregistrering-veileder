bbbb8e743ed86eac1b1fae2d1b79c415dfcfe0cf
Christian Rennemo <christian.rennemo@nav.no>
2026-03-06 15:30:17 +0100

    slettet ubrukt innhold fra lib

--- src/lib/lag-oppslagsapi-v2-kall.ts
+++
@@ -1,8 +0,0 @@
-import lagApiHandlerMedAuthHeaders, { ApiHandlerOpts, getOppslagsAPIV2Token } from './next-api-handler';
-import { NextApiHandler } from 'next';
-
-type LagOppslagsApiV2Kall = (url: string, opts: ApiHandlerOpts) => NextApiHandler;
-const lagOppslagsApiV2Kall: LagOppslagsApiV2Kall = (url, opts) =>
-    lagApiHandlerMedAuthHeaders(url, getOppslagsAPIV2Token, opts);
-
-export default lagOppslagsApiV2Kall;
