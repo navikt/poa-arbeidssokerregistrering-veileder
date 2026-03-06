ab623a70d7b8dc70c64887a95241324274a0eebb
Christian Rennemo <christian.rennemo@nav.no>
2026-03-06 15:01:12 +0100

    flytt isready isalive til app-router

--- src/pages/api/isready.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const isReadyHandler = (req: NextApiRequest, res: NextApiResponse<string>): void => {
-    res.status(200).json('Ready');
-};
-
-export default isReadyHandler;
