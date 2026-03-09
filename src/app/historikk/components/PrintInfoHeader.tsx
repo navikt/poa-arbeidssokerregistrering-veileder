b15b102b1f3687f94b49b446f882350ed4d1bce0
Christian Rennemo <christian.rennemo@nav.no>
2026-03-09 21:26:26 +0100

    fix hydration issue

--- src/app/historikk/components/PrintInfoHeader.tsx
+++ src/app/historikk/components/PrintInfoHeader.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import { prettyPrintDatoOgKlokkeslett } from '@/lib/date-utils';
 
 interface Props {
@@ -6,14 +8,19 @@ interface Props {
 }
 
 const PrintInfoHeader = (props: Props) => {
+    const [time, setTime] = useState<null | string>(null);
+
+    useEffect(() => {
+        const now = prettyPrintDatoOgKlokkeslett(new Date().toISOString(), 'nb', true);
+        setTime(now);
+    }, []);
+
     return (
         <section className={'hidden print:block w-full mt-4 mb-8 text-center'}>
             <div className={'flex flex-col content-center justify-center flex-wrap'}>
                 <Image src={'/nav-logo.svg'} alt={'Nav Logo'} width={100} height={50} />
             </div>
-            <div className={'mt-8 mb-4'}>
-                Utskriftsdato: {prettyPrintDatoOgKlokkeslett(new Date().toISOString(), 'nb', true)}
-            </div>
+            <div className={'mt-8 mb-4'}>Utskriftsdato: {time ?? ' '}</div>
             <div>Fødselsnummer: {props.fnr || '--'}</div>
         </section>
     );
